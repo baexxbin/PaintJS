81c77175f0e9bcf482571320076f7e9097247279
baexxbin <soo3886@gmail.com>
2021-02-09 22:37:27 +0900

    PaintJS end

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # PaintJS
+
+<setup 파트>     
+````
+> js : id 사용    
+> css : class 사용    
+````
+
+> *색깔팔레트 동그란 모양으로 만들기*    
+> " border-radius " 설정하기    
+> 이때, border-radius는 width의 1/2값으로 설정    
+> ex) width : 50, border-radius : 25       
+
+
+<css 파트>
+````
+> flex    
+>   * 컨테이너 적용    
+>        아이템들 가로방향 배치    
+>        자신이 가진 내용물의 width만큼 차지     
+>        ** flex-direction : 아이템들이 배치되는 축의 방향을 결정     
+>           메인축을 가로x축을 할지, 세로y축을 기준으로 할지, 이에따라 아이템들을 끼움    
+>       
+>       ** flex-wrap : 컨테이너가 더 이상 아이템들을 한 줄에 담을 여유 공간이 없을 때
+아이템 줄바꿈을 어떻게 할지 결정     
+````
+````     
+>
+>   * 아이템 적용  
+>       flex-basis : Flex 아이템의 기본 크기를 설정      
+>
+>
+>   all: unset; ==> 원래의 모양 초기화     
+>    padding: 50px 0px; ==> (위,아래) (오른쪽,왼쪽)       
+````    
+
+<js 파트>
+canvas는 context를 가짐.    
+context는 픽셀에 접근할 수 있는 방법, ==> canvas안의 픽셀들     
+이 픽셀들을 다룰 수 있는 공간인 width, height 지정해주기    
+
+* Array.from(colors)
+    : array.from매소드는 object로 부터 array를 만듦     
+
+* 링크 설정
+    * link.href => image(URL)이 되어야하고,     
+    * link.download => image의 이름이 됨 (저장할때 저장파일이름!)
---
+++ app.js
@@ -0,0 +1,106 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
+
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
+
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
+ctx.lineWidth = 2.5;
+
+let painting = false;
+let filling = false;
+
+function stopPainting(){
+    painting = false;
+}
+
+function startPainting(){
+    painting = true;
+}
+
+function onMouseMove(event){
+    const x = event.offsetX;
+    const y = event.offsetY;
+    if(!painting){          // 마우스를 클릭! 하고 움직이는 동안은 if실행X (else 실행)
+        ctx.beginPath();    // path만들기
+        ctx.moveTo(x, y);    //x,y 값으로 path 옮기기
+    } else{                  // 마우스를 움직이는 내내 발생
+        ctx.lineTo(x, y);    // path의 마지막 점에 연결(계속 작은 점들을 이어나가는 느낌)
+        ctx.stroke();  // 획 긋기
+    }
+}
+
+
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+}
+
+function handleRangeChange(event){
+    const size = event.target.value;
+    ctx.lineWidth = size; 
+}
+
+function handleModeClick(){
+    if(filling === true){
+        filling = false;
+        mode.innerText = "Fill";
+    } else{
+        filling = true;
+        mode.innerText = "Paint";
+    }
+}
+
+function handleCanvasClick(){
+    if(filling){
+        ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    }
+}
+
+function handleCM(event){
+    event.preventDefault();
+}
+
+function handleSaveClick(){
+    const image = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS🎨";
+    link.click();
+}
+
+if(canvas){
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM);
+}
+
+Array.from(colors).forEach(color =>
+    color.addEventListener("click", handleColorClick)
+);
+
+if(range){
+    range.addEventListener("input", handleRangeChange)
+}
+
+if(mode){
+    mode.addEventListener("click", handleModeClick)
+}
+
+if(saveBtn){
+    saveBtn.addEventListener("click", handleSaveClick);
+}
